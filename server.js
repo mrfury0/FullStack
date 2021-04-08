4d68a7f05bcb316d2de28a60a588f5c1a22836ba
mrfury0 <hannanfaryad@gmail.com>
2021-04-09 03:32:32 +0500

    Initial setup

---
+++ server.js
@@ -0,0 +1,49 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+
+const app = express();
+
+const expressLayouts = require("express-ejs-layouts");
+
+const index = require("./routes/index.js");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      connectTimeoutMS: 10000,
+      poolSize: 10,
+      writeConcern: {
+        j: true,
+      },
+    });
+    console.log("MongoDB Connected..");
+  } catch (err) {
+    console.error(err.message);
+
+    // Exit Process with Faliure
+    process.exit(1);
+  }
+};
+
+app.set("view engine", "ejs");
+
+app.set("views", __dirname + "/views");
+
+app.set("layout", "layouts/layouts");
+
+app.use(expressLayouts);
+
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+
+connectDB();
+
+app.use("/", index);
+
+app.listen(process.env.PORT || 3000);
